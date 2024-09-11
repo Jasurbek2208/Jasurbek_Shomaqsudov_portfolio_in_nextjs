3f81d278280d8108f1b2c59e7b05270885683147
Jasur <jasurbekshomaqsudov2004@gmail.com>
2024-09-11 11:58:15 +0500

    third commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import './globals.css'
+import Head from 'next/head'
+import Script from 'next/script'
 import type { Metadata } from 'next'
 import { StyledComponentsRegistry } from 'helpers'
 
 export const metadata: Metadata = {
   title: 'Jasurbek Shomaqsudov - Software Engineer',
   description: 'Jasurbek Shomaqsudov is a front-end developer who creates responsive and user-friendly websites. See my portfolio and learn more about my skills, experience.',
-  keywords: "Jasurbek Shomaqsudov, jasurbek shomaqsudov, Shomaqsudov Jasurbek, shomaqsudov jasurbek, shomaqsudov jasurbek frontend, shomaqsudov Jasurbek, shomaqsudov jasur, shomaqsudov, jasurShomaqsudov, JasurShomaqsudov, jasurshomaqsudov, Jasurshomaqsudov_jasur, jasur, jasur shomaqsudov, shomaqsudov, front-end developer, Jasurbek Shomaqsudov, jasurbek_shomaqsudov, jasur_shomaqsudov, Jasurbek's Portfolio, jasur shomaqsudov, Jasur Shomaqsudov, Jasurbek Frontend, Jasurbek Frontend Developer, Frontend Developer, jasurbekshomaqsudov, jasurshomaqsudov, shomaqsudovjasurbek, jasurbek shomaqsudov uz, shomaqsudovjasur, shomaqsudov_jasurbek_portfolio, shomaqsudov jasurbek portfolio, jasurbek, Jasurbek, JASURBEK, jasur, Jasur, JASUR, shomaqsudov, Shomaqsudov, SHOMAQSUDOV, j, a, s, u, r, b, e, k, ja, jasu, sho, shomaqsudo, jasur uz, jasuruz, jasurbekuz, shomaqsudov uz, shomaqsudovuz, jasur uzb, jasuruzb, jasurbekuzb, shomaqsudov uzb, shomaqsudovuzb, software engineer, jasur software engineer, jasurbek software engineer, shomaqsudov software engineer, programming, frontend, react developer, web developer, portfolio, HTML, CSS, JavaScript, jQuery, Bootstrap, responsive design, web design, web development, front-end, frontend, Джасурбек Шомаксудов, Джасурбек Шомаксудов, Шомаксудов Джасурбек, Шомаксудов Джасурбек, Шомаксудов Джасурбек фронтенд, Шомаксудов Джасурбек, Шомаксудов Джасур, Джасур, Джасур Шомаксудов, Шомаксудов, Front-end разработчик, Джасурбек Шомаксудов, Портфолио Джасурбека, Джасур Шомаксудов",
+  keywords:
+    "Jasurbek Shomaqsudov, jasurbek shomaqsudov, Shomaqsudov Jasurbek, shomaqsudov jasurbek, shomaqsudov jasurbek frontend, shomaqsudov Jasurbek, shomaqsudov jasur, shomaqsudov, jasurShomaqsudov, JasurShomaqsudov, jasurshomaqsudov, Jasurshomaqsudov_jasur, jasur, jasur shomaqsudov, shomaqsudov, front-end developer, Jasurbek Shomaqsudov, jasurbek_shomaqsudov, jasur_shomaqsudov, Jasurbek's Portfolio, jasur shomaqsudov, Jasur Shomaqsudov, Jasurbek Frontend, Jasurbek Frontend Developer, Frontend Developer, jasurbekshomaqsudov, jasurshomaqsudov, shomaqsudovjasurbek, jasurbek shomaqsudov uz, shomaqsudovjasur, shomaqsudov_jasurbek_portfolio, shomaqsudov jasurbek portfolio, jasurbek, Jasurbek, JASURBEK, jasur, Jasur, JASUR, shomaqsudov, Shomaqsudov, SHOMAQSUDOV, j, a, s, u, r, b, e, k, ja, jasu, sho, shomaqsudo, jasur uz, jasuruz, jasurbekuz, shomaqsudov uz, shomaqsudovuz, jasur uzb, jasuruzb, jasurbekuzb, shomaqsudov uzb, shomaqsudovuzb, software engineer, jasur software engineer, jasurbek software engineer, shomaqsudov software engineer, programming, frontend, react developer, web developer, portfolio, HTML, CSS, JavaScript, jQuery, Bootstrap, responsive design, web design, web development, front-end, frontend, Джасурбек Шомаксудов, Джасурбек Шомаксудов, Шомаксудов Джасурбек, Шомаксудов Джасурбек, Шомаксудов Джасурбек фронтенд, Шомаксудов Джасурбек, Шомаксудов Джасур, Джасур, Джасур Шомаксудов, Шомаксудов, Front-end разработчик, Джасурбек Шомаксудов, Портфолио Джасурбека, Джасур Шомаксудов",
   openGraph: {
     title: 'Jasurbek Shomaqsudov - Software Engineer',
     description: 'Jasurbek Shomaqsudov is a front-end developer who creates responsive and user-friendly websites. See my portfolio and learn more about my skills, experience.',
@@ -23,7 +26,7 @@ export const metadata: Metadata = {
   },
   applicationName: 'shomaqsudov.uz',
   authors: { name: 'Jasurbek Shomaqsudov', url: 'https://shomaqsudov.uz' },
-  // category: ['programming', 'sofware engineering', 'portfolio'],
+  category: 'Software engineering',
   twitter: {
     title: 'Jasurbek Shomaqsudov - Software Engineer',
     description: 'Jasurbek Shomaqsudov is a front-end developer who creates responsive and user-friendly websites. See my portfolio and learn more about my skills, experience.',
@@ -42,7 +45,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <head>
+      <Head>
         <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap' rel='stylesheet' />
         <link rel='canonical' href='https://shomaqsudov.uz' />
 
@@ -50,13 +53,22 @@ export default function RootLayout({
         <meta name='google-adsense-account' content='ca-pub-9331562588011655' />
         <meta name='google-site-verification' content='YKNgW6dch-cCV_lEa2PUcyteFd7APTHkNIwYbtH6JXc' />
 
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script async src='https://www.googletagmanager.com/gtag/js?id=G-V4LQXC2FYF' defer></script>
-        <script>
-          window.dataLayer = window.dataLayer || [] function gtag() {dataLayer?.push(arguments)}
-          gtag('js', new Date()) gtag('config', 'G-V4LQXC2FYF')
-        </script>
-      </head>
+        <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-V4LQXC2FYF' />
+        <Script
+          id='google-gtag'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-V4LQXC2FYF');
+          `,
+          }}
+        />
+      </Head>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
